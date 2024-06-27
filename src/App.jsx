4f4b4ea59eb01ec7634fbc994f62f89650318ffb
ArpitKaul <akaul7407@gmail.com>
2024-06-27 15:27:12 +0530

    creating header and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
 import Header from "./Components/Header";
+import  Hero from "./Components/Hero";
 const App = () => {
 
   return (
@@ -7,6 +8,7 @@ const App = () => {
     
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero/>
       </div>
       <ButtonGradient/>
     </>
